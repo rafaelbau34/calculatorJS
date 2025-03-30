5d4adc8a7edab80cf6704808f860a34b2414af8a
rafael <rafaelbaugar@gmail.com>
2025-03-30 03:41:34 -0600

    some ui adjustments

---
+++ main.js
@@ -0,0 +1,11 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearr() {
+  display.value = "";
+}
+
+function calculate() {}
