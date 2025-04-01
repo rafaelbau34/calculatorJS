121bc74643afcd154b75fc0717f638162c1debc0
rafael <rafaelbaugar@gmail.com>
2025-04-01 01:39:14 -0600

    this calculator works and rocks

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let display = document.getElementById("display");
 
 function appendToDisplay(input) {
+  let lastChar = display.value.slice(-1);
+  const operators = ["+", "-", "/", "*", "%"];
+  if (display.value === "" && operators.includes(input)) return;
+  if (operators.includes(input) && operators.includes(lastChar)) return;
+  if (input == "." && display.value.match(/[\d]*\.[\d]*$/)) return;
   display.value += input;
 }
 
@@ -8,4 +13,15 @@ function clearr() {
   display.value = "";
 }
 
-function calculate() {}
+function calculate() {
+  try {
+    let result = new Function("return " + display.value)();
+    display.value = result;
+  } catch {
+    display.value = "ERROR";
+  }
+}
+
+function del() {
+  display.value = display.value.slice(0, -1);
+}
